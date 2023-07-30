86444248089ce5d4f16b0731b39003da41bdb204
Eli-J-Paris <Eli.J.Paris@gmail.com>
2023-07-30 12:35:15 -0600

    Added CRUD minus delete to countries

---
+++ CrudBucketMVC/Controllers/CountriesController.cs
@@ -0,0 +1,79 @@
+﻿using CrudBucketMVC.DataAccess;
+using CrudBucketMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudBucketMVC.Controllers
+{
+    public class CountriesController : Controller
+    {
+        private readonly CrudBucketContext _context;
+
+        public CountriesController(CrudBucketContext context)
+        {
+            _context = context;
+        }
+
+        [Route("/contienents/{contienentId:int}/countries")]
+        public IActionResult Index(int contienentId)
+        {
+            var contienent = _context.Contienents.Where(c => c.Id == contienentId).Include(c => c.Countries).First();
+            return View(contienent);
+        }
+
+        [Route("/contienents/{contienentId:int}/countries/new")]
+        public IActionResult New(int contienentId)
+        {
+            var contienent = _context.Contienents
+                .Where(c => c.Id == contienentId)
+                .Include(c => c.Countries)
+                .First();
+
+            return View(contienent);
+        }
+
+        [HttpPost]
+        [Route("/contienents/{contienentId:int}/countries")]
+        public IActionResult Create(Country country, int contienentId)
+        {
+            var contienent = _context.Contienents
+                .Where(c => c.Id == contienentId)
+                .Include(c => c.Countries)
+                .First();
+
+            contienent.Countries.Add(country);
+            _context.Countries.Add(country);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", new { contienentId = contienent.Id });
+        }
+
+        [Route("/contienents/{id:int}/countries/{countryId:int}/edit")]
+        public IActionResult Edit(int id, int countryId)
+        {
+            var contienent = _context.Contienents.Find(id);
+            var country = _context.Countries.Find(countryId);
+
+            country.contienent = contienent;
+
+            return View(country);
+        }
+
+
+        [HttpPost]
+        [Route("/contienents/{contienentId:int}/countries/{countryid:int}")]
+        public IActionResult Update(Country country, int countryid, int contienentId)
+        {
+            var contienent = _context.Contienents.Find(contienentId);
+
+            country.contienent = contienent;
+            country.Id = countryid;
+            _context.Countries.Update(country);
+            _context.SaveChanges();
+
+
+            return RedirectToAction("Index", new { contienentId = contienent.Id });
+        }
+
+    }
+}
