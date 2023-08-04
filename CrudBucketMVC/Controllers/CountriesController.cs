a0836649433adba31b382c8c48eea06f444e5eee
Eli-J-Paris <Eli.J.Paris@gmail.com>
2023-08-04 09:58:01 -0600

    CRUD for countries and contienets

--- CrudBucketMVC/Controllers/CountriesController.cs
+++ CrudBucketMVC/Controllers/CountriesController.cs
@@ -71,9 +71,25 @@ namespace CrudBucketMVC.Controllers
             _context.Countries.Update(country);
             _context.SaveChanges();
 
-
             return RedirectToAction("Index", new { contienentId = contienent.Id });
         }
+
+
+
+        [HttpPost]
+        [Route("/countries/{contienentId:int}/countries/{countryid:int}/delete")]
+        public IActionResult Delete(int contienentId, int countryid)
+        {
+            var contienent = _context.Contienents.Find(contienentId);
+
+            var country = _context.Countries.Find(countryid);
+            _context.Countries.Remove(country);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", new { contienentId = contienent.Id });
+
+        }
+
 
     }
 }
